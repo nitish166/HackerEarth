2744904cd46daaf455070e8d70b280febef96421
nitish166 <knitish123@gmail.com>
2019-10-15 17:13:49 +0530

    Array Updated Problem

--- Array/Triplets.cpp
+++ Array/Triplets.cpp
@@ -1,34 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 int main()
 {
-	int n;
+	ll n;
 	cin>>n;
-	int* arr = new int[n];
-	for(int i=0;i<n;i++)
+	ll* arr = new ll[n];
+	ll ones, twos, threes, others;
+	ones = twos = threes = others = 0;
+	
+	for (int i = 0;i < n;i++)
 	{
-		cin>>arr[i];
+		cin >> arr[i];
+		if (arr[i] == 1)ones++;
+		else if (arr[i] == 2)twos++;
+		else if (arr[i] == 3)threes++;
+		else others++;
 	}
-   // sort(arr, arr+n);
-    int sum=0;
-	for(int i=0;i<n-2;i++)
-	{
-		int l,r,k;
-		l=i+1;
-		k=l+1;
-		r=n-1;
-
-		while(l<r)
-		{
-			sum +=floor((arr[i]+arr[l]+arr[k])/(arr[i]*arr[l]*arr[k]));
-			l++;
-			r--;
-		}
-
-	}
-	cout<<sum<<endl;
+	
+	ll ans = 0;
+	//case: 1 1 1
+	ans = ((ones * (ones - 1) * (ones - 2)) / 2);
+	//case: 1 1 2
+	ans += ((ones*(ones - 1)))*(twos);
+	//case: 1 1 x
+	ans += ((ones*(ones - 1))/2)*(others+threes);
+	//case: 1 2 2
+	ans += ((twos*(twos - 1))/2)*(ones);
+	//case: 1 2 3
+	ans += (ones*twos*threes);
+	cout << ans << endl;
 	return 0;
-
-
-
 }

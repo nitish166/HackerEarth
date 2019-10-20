f8a426f52640bdcab564b2aa6dcc607ed70a5663
nitish166 <knitish123@gmail.com>
2019-10-20 10:28:28 +0530

    Updated

--- Greedy/Activity.cpp
+++ Greedy/Activity.cpp
@@ -2,15 +2,24 @@
 using namespace std;
 
 
-int MaximumActivity(int* arr1, int* arr2, int n)
+struct Activity{
+	int start, finish;
+};
+
+
+bool compare(Activity s1, Activity s2)
+{
+	return (s1.finish < s2.finish);
+}
+
+int CountMaximumActivity(Activity* arr, int n)
 {
 	int i=0;
-	int count =1;
-
-    sort(arr2, arr2+n);
-	for(int j=1; j<n; j++)
+	int count=1;
+	sort(arr, arr+n, compare);
+	for(int j=1;j<n;j++)
 	{
-		if(arr1[j]>=arr2[i])
+		if(arr[j].start>=arr[i].finish)
 		{
 			count++;
 			i=j;
@@ -24,17 +33,15 @@ int main()
 {
 	int n;
 	cin>>n;
-	int* arr1 = new int[n];
-	int* arr2 = new int[n];
-
+	
+	Activity* arr = new Activity[n];
 	for(int i=0;i<n;i++)
 	{
-		cin>>arr1[i];
-		cin>>arr2[i];
+		cin>>arr[i].start;
+		cin>>arr[i].finish;
 	}
-	
 
-	int ans = MaximumActivity(arr1, arr2, n);
+	int ans = CountMaximumActivity(arr, n);
 	cout<<ans<<endl;
 	return 0; 
 }
